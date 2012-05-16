113df69f70babf3de8a196fcc4b47db7b84c4332
Rob Garland <robsgarland@gmail.com>
2012-05-15 23:54:00 -0400

    first commit

---
+++ TestGame/TestGame/Map.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TestGame
+{
+    class Map
+    {
+        public int x_size;
+        public int y_size;
+        public int[,] map;
+
+        public Map(int x, int y)
+        {
+            x_size = x;
+            y_size = y;
+            map = new int[x_size,y_size];
+
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < y_size; y++)
+            {
+                for (int x = 0; x < x_size; x++)
+                {
+                    //tile.Draw(x,y);
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ TestGame/TestGame/Tile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace TestGame
+{
+    class Tile
+    {
+        public Texture2D icon;
+        public int layer;
+        public Vector2 position;
+        public int height;
+        public int width;
+
+        public Tile(string fname,ContentManager content)
+        {
+            icon = content.Load<Texture2D>(fname);
+            layer = 0;
+            height = icon.Height;
+            width = icon.Width;
+            
+        }
+
+        public void Draw(int x, int y, SpriteBatch spriteBatch)
+        {
+            Rectangle tile = new Rectangle(x,y,x+width,y+height);
+            spriteBatch.Draw(icon,tile,Color.White);
+        }
+
+        public void DrawRel(int x, int y, SpriteBatch spriteBatch)
+        {
+            Rectangle tile = new Rectangle(x * width+1, y * height+1, x + width, y + height);
+            spriteBatch.Draw(icon, tile, Color.White);
+            //position = new Vector2(x , y);
+            
+        }
+
+    }
+
+
+}
